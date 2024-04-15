ebdceb2c376f046b23fd920d942dcaa16edf74bd
amirani <amirani>
2024-04-15 18:30:41 +0400

    feat: create Cart

---
+++ src/app/cart.tsx
@@ -0,0 +1,33 @@
+import Button from "@/components/Button";
+import CartListItem from "@/components/CartListItem";
+import { useCart } from "@/providers/CartProvider";
+import { FlatList, Text, View } from "react-native";
+
+const CartScreen = () => {
+  const { items, total } = useCart();
+
+  if (!items.length) {
+    return (
+      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+        <Text style={{ fontSize: 24, color: "purple" }}>Cart is empty</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{ gap: 10 }}
+      />
+
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "500" }}>
+        Total: ${total}
+      </Text>
+      <Button text="Checkout" />
+    </View>
+  );
+};
+
+export default CartScreen;
