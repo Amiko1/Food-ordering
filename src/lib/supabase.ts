4cc1cb23d11e541e1e1b4fe067ce46b8233b848d
amirani <amirani>
2024-04-19 20:51:10 +0400

    feat: setup supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://vvxkoccrzgyherjcbkfn.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2eGtvY2Nyemd5aGVyamNia2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1NDUwNDIsImV4cCI6MjAyOTEyMTA0Mn0.NCQq7YdhIMNrfI_SOcpocOvcudNvlSb5F5rwSatRxAg";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
