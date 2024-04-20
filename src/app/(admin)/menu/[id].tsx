00a735df28d3046d29f8c390f7335bb170c541db
amirani <amirani>
2024-04-20 17:02:36 +0400

    feat: fetch produc on admin side

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,25 +1,34 @@
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import React, { useState } from "react";
 import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 
 import Button from "@components/Button";
 import { useCart } from "@/providers/CartProvider";
 import { PizzaSize } from "@/types";
-import products from "@assets/data/products";
 import { FontAwesome } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
+import { useProduct } from "@/app/api/products";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
 const ProductDetailsScreen = () => {
-  const { id } = useLocalSearchParams();
+  const { id: idString } = useLocalSearchParams();
+  const id = parseFloat(typeof idString === "string" ? idString : idString[0]);
+
+  const { data: product, error, isLoading } = useProduct(id);
   const { addItem } = useCart();
   const router = useRouter();
 
   const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
-  const product = products.find((p) => p.id.toString() === id);
 
   const addToCart = () => {
     if (!product) return;
@@ -27,7 +36,10 @@ const ProductDetailsScreen = () => {
     router.push("/cart");
   };
 
-  if (!product) {
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
     return <Text>Failed to fetch products</Text>;
   }
 
@@ -51,9 +63,9 @@ const ProductDetailsScreen = () => {
           ),
         }}
       />
-      <Stack.Screen options={{ title: product.name }} />
+      <Stack.Screen options={{ title: product?.name }} />
       <Image
-        source={{ uri: product.image || defaultPizzaImage }}
+        source={{ uri: product?.image || defaultPizzaImage }}
         style={styles.image}
         resizeMode="contain"
       />
@@ -82,7 +94,7 @@ const ProductDetailsScreen = () => {
           </Pressable>
         ))}
       </View>
-      <Text style={styles.price}>Price: ${product.price.toFixed(2)}</Text>
+      <Text style={styles.price}>Price: ${product?.price.toFixed(2)}</Text>
       <Button onPress={addToCart} text="Add to cart" />
     </View>
   );
